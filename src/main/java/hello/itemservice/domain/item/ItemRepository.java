fc07aa06148c2bdcd4c2bbf2602250fa0464c0a6
jinkwon9301 <valor9254@gmail.com>
2023-04-05 17:21:10 +0900

    상품 목록 : 타임리프

--- src/main/java/hello/itemservice/domain/item/ItemRepository.java
+++ src/main/java/hello/itemservice/domain/item/ItemRepository.java
@@ -11,7 +11,7 @@ import java.util.Map;
 public class ItemRepository { // 테스트 짜보기 : cmd + shift + T
 
   private static final Map<Long, Item> store = new HashMap<>(); // static, (실제는 HashMap 사용하면 안된다 : 멀티쓰레드 환경에서 여러개가 store에 동시에 접근하는 상황이라면 HashMap 사용 X (singleton으로 생성되는 상황). 대신 ConcurrentHashMap<>() 사용해야 한다.)
-  private static long sequence = 0L; // static
+  private static long sequence = 1L; // static
 
   public Item save(Item item) {
     item.setId(sequence++);
